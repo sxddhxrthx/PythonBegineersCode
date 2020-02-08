bf113a7bfffe69f5656c84ba4d870046d69bb496
Siddhartha Chowdhury <sidc.117@gmail.com>
2020-02-08 06:13:08 +0000

    difference forms of string slicing

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 str = "hello world"
 print(str[0])
+
+# outputs - h
+
+print(str[8])
+
+# outputs - 4
+
+print(str[2:])
+
+# outputs - llo world
+# starts from index 2 and ends at last Index
+
+print(str[:4])
+
+# outputs - hell
+# starts from starting index and ends at index n-1
+
+print(str[3:7])
